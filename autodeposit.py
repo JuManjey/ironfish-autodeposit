332ec30971df1b6ef400287094686b7360e16478
JuManji <opghld@gmail.com>
2022-05-15 14:13:00 +0300

    Update autodeposit.py

--- autodeposit.py
+++ autodeposit.py
@@ -16,3 +16,8 @@ else:
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output)
+    import datetime
+    e = datetime.datetime.now()
+    print ("Current date and time = %s" % e)
+    print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
+    print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
