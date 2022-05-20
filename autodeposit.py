a569369c220d21acde82455a8f3ff9ae4d882b9b
JuManji <opghld@gmail.com>
2022-05-21 01:52:28 +0300

    Version 2
    add telegram bot autentification

--- autodeposit.py
+++ autodeposit.py
@@ -1,23 +1,23 @@
-from operator import eq
 import subprocess
+import datetime
+import os
+import struct
+from time import sleep
 
 print("Start of work")
+e = datetime.datetime.now()
 bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "accounts:balance"]
 process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
 output, error = process.communicate()
-massive2 = output.split()
-
-massive_of_spend_balance = ((str)(massive2[36])).strip()
-if ((str)(massive_of_spend_balance[2])) == "0":             
-    print("Выход! Баланса недостаточно")
-else:                    
-    print("Выполнение операции депозита")
+massive2 = output.split();
+count = float(massive2[39])
+name_of_client = str(massive2[25].decode("utf-8"))
+if count < 1:
+    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Выход\ !" + "Баланса\ недостаточно\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
+    sleep(300)
+    exit()
+else:
+    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Депозит\ !" + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
     output, error = process.communicate()
-    print(output)
-    import datetime
-    e = datetime.datetime.now()
-    print ("Current date and time = %s" % e)
-    print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
-    print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
