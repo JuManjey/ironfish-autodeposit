6a1c0d451b9096539ff33e3af685eacf60393902
JuManji <opghld@gmail.com>
2022-05-22 02:47:00 +0300

    Update autodeposit.py

--- autodeposit.py
+++ autodeposit.py
@@ -9,15 +9,16 @@ e = datetime.datetime.now()
 bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "accounts:balance"]
 process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
 output, error = process.communicate()
-massive2 = output.split();
-count = float(massive2[39])
-name_of_client = str(massive2[25].decode("utf-8"))
+massive = output.decode("utf-8")
+massive2 = massive.split()
+count = float(massive2[36])
+name_of_client = str(massive2[22])
 if count < 1:
-    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Выход\ !" + "Баланса\ недостаточно\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
+    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Выход\ " + "Баланса\ недостаточно\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     sleep(300)
     exit()
 else:
-    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Депозит\ !" + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
+    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Депозит\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
     output, error = process.communicate()
