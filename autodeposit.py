186c83b6846d914974bc1d4a95562a649ef4bcaf
JuManji <opghld@gmail.com>
2022-05-23 17:50:02 +0300

    Update autodeposit.py

--- autodeposit.py
+++ autodeposit.py
@@ -14,11 +14,11 @@ massive2 = massive.split()
 count = float(massive2[36])
 name_of_client = str(massive2[22])
 if count < 1:
-    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Выход\ " + "Баланса\ недостаточно\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
+    os.system("curl https://api.telegram.org/bot5291389******/sendMessage?chat_id=178*** -d text=Выход\ " + "Баланса\ недостаточно\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     sleep(300)
     exit()
 else:
-    os.system("curl https://api.telegram.org/bot5291389954:AAE-PIYhNxt_yRBwioZPjP2T6DS2IQDpf6E/sendMessage?chat_id=1740882388 -d text=Депозит\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
+    os.system("curl https://api.telegram.org/bot5291389******/sendMessage?chat_id=178*** -d text=Депозит\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
     output, error = process.communicate()
