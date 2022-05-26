2fbb3962ab4eafb802d6f492d277ede116e8b68d
JuManji <opghld@gmail.com>
2022-05-26 17:47:53 +0300

    Update autodeposit.py

--- autodeposit.py
+++ autodeposit.py
@@ -5,15 +5,22 @@ import struct
 from time import sleep
 
 print("Start of work")
-bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "accounts:balance"]
+bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/",
+               "start", "accounts:balance"]
 process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
 output, error = process.communicate()
 massive = output.decode("utf-8")
 massive2 = massive.split()
 count = float(massive2[36])
 name_of_client = str(massive2[22])
-if count > 1:
+status = os.system('systemctl is-active --quiet service-name')
+if count > 1 and status != 0:
+    bashCommand = ["/etc/init.d/cron", "start"]
+    sleep(5)
+    bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/",
+                   "start", "deposit", "--confirm"]
     os.system("curl https://api.telegram.org/bot5291389******/sendMessage?chat_id=178*** -d text=Депозит\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
-    bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
-exit();
+else:
+    bashCommand = ["/etc/init.d/cron", "stop"]
+exit()
