c4b7b47e8efd05c5a79bb42fbc6c748e3a69f939
Sergey Manzhos <opghld@gmail.com>
2022-08-07 17:54:12 +0300

    Update autodeposit.py

--- autodeposit.py
+++ autodeposit.py
@@ -12,15 +12,11 @@ output, error = process.communicate()
 massive = output.decode("utf-8")
 massive2 = massive.split()
 count = float(massive2[37])
-name_of_client = str(massive2[22])
-status = os.system('systemctl is-active --quiet service-name')
-if count > 1 and status != 0:
-    bashCommand = ["/etc/init.d/cron", "start"]
-    sleep(5)
-    bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/",
-                   "start", "deposit", "--confirm"]
+name_of_client = str(massive2[23])
+if count > 1:
+    bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
     os.system("curl https://api.telegram.org/bot5291389******/sendMessage?chat_id=178*** -d text=Депозит\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
 else:
-    bashCommand = ["/etc/init.d/cron", "stop"]
+    os.system("curl https://api.telegram.org/bot5586274322:AAHvvLCE5ml***/sendMessage?chat_id=1740*** -d text=Not enaugh money \ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
 exit()
