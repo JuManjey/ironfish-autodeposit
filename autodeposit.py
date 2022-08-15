ee8e4c315e2d580b06251a235a30f5b1b8e69f6b
Sergey Manzhos <opghld@gmail.com>
2022-08-15 17:38:34 +0300

    Update autodeposit.py

--- autodeposit.py
+++ autodeposit.py
@@ -16,7 +16,4 @@ if count > 1:
     bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
     os.system("curl https://api.telegram.org/bot5291389******/sendMessage?chat_id=178*** -d text=Депозит\ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
-#else:
-#    os.system("curl https://api.telegram.org/bot5586274322:AAHvvLCE5ml***/sendMessage?chat_id=1740*** -d text=Not enaugh money \ " + str(name_of_client) + "\ У\ Вас\ на\ балансе\ " + str(count))
-#    process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
 exit()
